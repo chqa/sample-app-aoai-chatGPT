0d5bdad32dfb579646d4a36580a0e70a1b531e9b
陈强 <36105155@qq.com>
2023-07-21 19:34:30 +0800

    title

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -173,8 +173,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>开始聊天</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>有什么问题问我呢？</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
